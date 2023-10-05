1968afd6c35779226f0a75d157f984755aa42d24
Alexander Emelyanov <Alexaneder.Emelyanov@kaspersky.com>
2023-10-05 07:16:37 +0300

    update config

--- highlights.lua
+++ highlights.lua
@@ -1,7 +1,21 @@
 -- To find any highlight groups: "<cmd> Telescope highlights"
 -- Each highlight group can take a table with variables fg, bg, bold, italic, etc
 -- base30 variable names can also be used as colors
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
 
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd [[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]]
+  
 local M = {}
 
 ---@type Base46HLGroupsList
--- init.lua
+++ init.lua
@@ -1,7 +1,29 @@
+require('base')
+
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
+
 autocmd("VimResized", {
   pattern = "*",
   command = "tabdo wincmd =",
 })
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_linux = has "unix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac == 1 then
+  require('enslit.macos')
+end
+if is_linux == 1 then
+  require('enslit.linux')
+end
+if is_win == 1 then
+  require('enslit.windows')
+end
+if is_wsl == 1 then
+  require('enslit.wsl')
+end
